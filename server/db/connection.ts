6e50144cc3f06aabf668d71f6c995fa01fe4636b
Regina Gyimesi <regina.gyimesi@marshmallow.co>
2025-09-13 12:28:50 +0200

    feat: add DB schema, diagram, justifications and refactor to ts

---
+++ server/db/connection.ts
@@ -0,0 +1,33 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db: any = null;
+
+async function connectToDatabase() {
+  if (db) {
+    return db;
+  }
+  
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB");
+    db = client.db("user_journey_tracker");
+    return db;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+export default connectToDatabase;
